e1fd44c017de17c35bbd197533a93862232b3942
Marijana49 <marijanazaric8@gmail.com>
2025-09-21 14:10:00 +0200

    klasa za slanje više pacijenata

---
+++ Klase/PaketPacijenata.cs
@@ -0,0 +1,10 @@
+﻿using Klase;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class PaketPacijenata
+{
+    public List<Pacijent> Urgentni { get; set; }
+    public List<Pacijent> Ostali { get; set; }
+}
